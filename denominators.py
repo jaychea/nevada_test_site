d4fc5163de8a183627244943b9c6e605e9fe658f
jaychea <64818255+jaychea@users.noreply.github.com>
2020-05-11 15:53:26 -0400

    Update denominators.py

--- denominators.py
+++ denominators.py
@@ -4,3 +4,6 @@ for i in range(1, num+1,1):
     y = num % i
     if y == 0:
         print(str(x))
+        
+# % is the modulus operator. If the numbers divided have a remainder, it will print the remainder.
+# Therefore, if the remainder of a number is 0, it is a whole number/integer. 
